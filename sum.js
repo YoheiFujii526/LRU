604c1eb8230ebb06f543f96642b29c67db38b43d
Yohei Fujii <youheifujii624@gmail.com>
2024-05-16 23:58:05 +0900

    初クラスのテスト

--- sum.js
+++ sum.js
@@ -1,4 +1,17 @@
-function sum(a, b) {
-    return a + b;
+//lrucacheクラスを作成
+class LRUcache {
+  //max_capacityは最大容量
+  constructor(max_capacity) {
+    this.max_capacity = max_capacity;
   }
-  module.exports = sum;
+  //メッソドの実験
+  increment() {
+    this.max_capacity++;
+    return this.max_capacity;
+  }
+}
+
+/*function sum(a, b) {
+    return a + b;
+}*/
+module.exports = LRUcache;
