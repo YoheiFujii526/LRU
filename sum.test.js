8d3b7e04f6d7d19e71401be2d23af398f19798d6
Yohei Fujii <youheifujii624@gmail.com>
2024-05-18 02:41:57 +0900

    プログラムが完成しテストも通過した

--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,18 @@
-const LRUcache = require('./sum');
+const Lrucache = require('./sum');
 
-test('adds 1 + 2 to equal 3', () => {
-  const lrucache = new LRUcache(2);
-  expect(lrucache.increment()).toBe(3);
+test('三つのデータを追加して一番最初に追加したデータを取り出そうとした時', () => {
+  const lrucache = new Lrucache(2);
+  lrucache.put("a", "dataA");
+  lrucache.put("b", "dataB");
+  lrucache.put("c", "dataC");
+  expect(lrucache.get("a")).toBe(null);
+});
+
+test('dataA追加→dataB追加→dataA取り出し→dataC追加→dataB取り出し', () => {
+  const lrucache = new Lrucache(2);
+  lrucache.put("a", "dataA");
+  lrucache.put("b", "dataB");
+  expect(lrucache.get("a")).toBe("dataA");
+  lrucache.put("c", "dataC");
+  expect(lrucache.get("b")).toBe(null);
 });
